3a7b0a681ce63439d13221718665849c5dd0160d
warishshahbaz <95901915+warishshahbaz@users.noreply.github.com>
2024-02-07 23:57:07 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,35 @@
 // Your code here.
+document.addEventListener('DOMContentLoaded', () => {
+  const items = document.querySelectorAll('.item');
+
+  let isDown = false;
+  let startX;
+  let scrollLeft;
+
+  items.forEach(item => {
+    item.addEventListener('mousedown', e => {
+      isDown = true;
+      item.classList.add('active');
+      startX = e.pageX - item.offsetLeft;
+      scrollLeft = item.scrollLeft;
+    });
+
+    item.addEventListener('mouseleave', () => {
+      isDown = false;
+      item.classList.remove('active');
+    });
+
+    item.addEventListener('mouseup', () => {
+      isDown = false;
+      item.classList.remove('active');
+    });
+
+    item.addEventListener('mousemove', e => {
+      if (!isDown) return;
+      e.preventDefault();
+      const x = e.pageX - item.offsetLeft;
+      const walk = (x - startX) * 2;
+      item.scrollLeft = scrollLeft - walk;
+    });
+  });
+});
